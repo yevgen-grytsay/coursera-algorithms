18da932abf3642c7a88fd67ae0549db768c467b0
Yevgen Grytsay <yevgen_grytsay@mail.ru>
2017-02-23 21:07:03 +0200

    Определение соседей

--- w4-8-puzzle/Board.java
+++ w4-8-puzzle/Board.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Iterator;
+
 /**
  * Created by yevgen on 22.02.17.
  */
@@ -8,6 +11,10 @@ public class Board {
     public Board(int[][] blocks) {
         this.blocks = blocks;
         n = blocks.length;
+    }
+
+    public int dimension() {
+        return n;
     }
 
     public int hamming() {
@@ -40,11 +47,83 @@ public class Board {
         return distance;
     }
 
+    public boolean equals(Object y) {
+        return Arrays.deepEquals(blocks, ((Board)y).blocks);
+    }
+
+    public Iterable<Board> neighbors() {
+        return new Iterable<Board>() {
+            @Override
+            public Iterator<Board> iterator() {
+                return new NeighbourIterator();
+            }
+        };
+    }
+
     private int row(int index) {
         return index / n;
     }
 
     private int col(int index) {
         return index % n;
+    }
+
+    private class NeighbourIterator implements Iterator<Board> {
+        private int spaceRow;
+        private int spaceCol;
+        private int i;
+        private int[][] nb;
+
+        public NeighbourIterator() {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (blocks[i][j] == 0) {
+                        spaceRow = i;
+                        spaceCol = j;
+                        break;
+                    }
+                }
+            }
+            int[][] sides = new int[][] {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+            int[][] tempNb = new int[4][2];
+            int i = 0;
+            for (int[] side: sides) {
+                int row = spaceRow + side[0];
+                int col = spaceCol + side[1];
+                if (validIndex(row) && validIndex(col)) {
+                    tempNb[i++] = new int[]{row, col};
+                }
+            }
+            nb = Arrays.copyOf(tempNb, i);
+        }
+
+        private boolean validIndex(int index) {
+            return index >= 0 && index < n;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return i <= nb.length;
+        }
+
+        @Override
+        public Board next() {
+            int row = nb[i][0];
+            int col = nb[i][1];
+            int[][] neighbor = copy(blocks);
+            neighbor[spaceRow][spaceCol] = neighbor[row][col];
+            neighbor[row][col] = 0;
+            ++i;
+
+            return new Board(neighbor);
+        }
+
+        private int[][] copy(int[][] input) {
+            int[][] target = new int[input.length][];
+            for (int i=0; i <input.length; i++) {
+                target[i] = Arrays.copyOf(input[i], input[i].length);
+            }
+            return target;
+        }
     }
 }
