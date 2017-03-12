dddff9b4a923ed5aab77d2c2c17bc177a739ef10
Yevgen Grytsay <yevgen_grytsay@mail.ru>
2017-03-12 22:31:20 +0200

    Небольшие исправления (87%)

--- w5-kd-trees/KdTree.java
+++ w5-kd-trees/KdTree.java
@@ -1,7 +1,4 @@
-import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.Point2D;
-import edu.princeton.cs.algs4.RectHV;
-import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.*;
 
 import java.util.Comparator;
 import java.util.LinkedList;
@@ -43,7 +40,7 @@ public class KdTree {
             }
         }
 
-        public Comparator<Point2D> comparator() {
+        public Comparator<Point2D> getLevelSpecificComparator() {
             return level % 2 == 0 ? Point2D.X_ORDER : Point2D.Y_ORDER;
         }
 
@@ -94,6 +91,7 @@ public class KdTree {
     }
 
     public void insert(Point2D p) {
+        if (contains(p)) return;
         Objects.requireNonNull(p);
         root = insert(root, p, 0, new RectHV(0, 0, 1, 1), root);
         ++size;
@@ -101,7 +99,7 @@ public class KdTree {
 
     private Node insert(Node x, Point2D key, int level, RectHV rect, Node parent) {
         if (x == null) return new Node(key, level, rect, parent);
-        Comparator<Point2D> cmp = x.comparator();
+        Comparator<Point2D> cmp = x.getLevelSpecificComparator();
 
         if (cmp.compare(key, x.key) < 0) x.left = insert(x.left, key,  level + 1, x.leftRect(), x);
         else x.right = insert(x.right, key,  level + 1, x.rightRect(), x);
@@ -110,16 +108,16 @@ public class KdTree {
 
     public boolean contains(Point2D p) {
         Objects.requireNonNull(p);
-        return contains(root, p, 0);
+        return contains(root, p);
     }
 
-    private boolean contains(Node x, Point2D p, int level) {
+    private boolean contains(Node x, Point2D p) {
         if (x == null) return false;
         if (x.key.equals(p)) return true;
 
-        Comparator<Point2D> cmp = x.comparator();
-        if (cmp.compare(p, x.key) < 0) return contains(x.left, p,  level + 1);
-        else return contains(x.right, p,  level + 1);
+        Comparator<Point2D> cmp = x.getLevelSpecificComparator();
+        if (cmp.compare(p, x.key) < 0) return contains(x.left, p);
+        else return contains(x.right, p);
     }
 
     public void draw() {
@@ -155,7 +153,7 @@ public class KdTree {
         }
 
         Node first, second;
-        int cmp = x.comparator().compare(p, x.key);
+        int cmp = x.getLevelSpecificComparator().compare(p, x.key);
         if (cmp < 0) {
             first = x.left;
             second = x.right;
@@ -165,7 +163,7 @@ public class KdTree {
         }
 
         Point2D nf = nearest(first, p, nearest);
-        if (nf != null && p.distanceTo(nf) < bestDistance && !nf.equals(p)) {
+        if (nf != null && p.distanceTo(nf) < bestDistance) {
             nearest = nf;
             bestDistance = p.distanceTo(nf);
         }
@@ -181,6 +179,5 @@ public class KdTree {
     }
 
     public static void main(String[] args) {
-
     }
 }
